fd603d508e40bc4792a455807112a5d59a52cf61
Sumi097 <159305850+Sumi097@users.noreply.github.com>
2024-07-04 20:26:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// textChange
+function textChange(){
+    document.getElementById('text_change').innerHTML="Life is beautiful";
+}
+
+
+function fontSize(){
+    document.getElementById('fontSizCchange').style.fontSize='60px';
+}
+function hide(){
+    document.getElementById('fontChange').style.display="none";
+}
+
+function show(){
+    document.getElementById('show_item').style.display="block";
+}
+
+function background(){
+    document.getElementById('bkChange').style.background='red';
+}
+// function turnOn() {
+//     document.getElementById('myImage').src='pic_bulboff.gif';
+// }
